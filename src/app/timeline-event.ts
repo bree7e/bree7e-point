ecdf74c39ea16086dadb8bd3fc9bd75119b6b2f4
Alexandr Vetrov <vetrov@icc.ru>
2018-05-13 01:06:51 +0800

    Добавлены классы новостей и транзакций

--- src/app/timeline-event.ts
+++ src/app/timeline-event.ts
@@ -1,10 +1,38 @@
+import { Currencies } from './currencies.enum';
+
 export abstract class TimelineEvent {
-    constructor(
-        private id: string,
-        public date: Date
-    ) { }
+    constructor(private _id: string, public date: Date) {}
+
+    get id(): string {
+        return this._id;
+    }
 
     get type() {
         return this.constructor.name;
+    }
+}
+
+export class NewsEvent extends TimelineEvent {
+    constructor(
+        id: string,
+        public title: string,
+        public content: string,
+        public isRead = false,
+        date: Date = new Date()
+    ) {
+        super(id, date);
+    }
+}
+
+export class TransactionEvent extends TimelineEvent {
+    constructor(
+        id: string,
+        date: Date,
+        public currency = Currencies.RUB,
+        public amount: number,
+        public agent: string,
+        public description: string = ''
+    ) {
+        super(id, date);
     }
 }
