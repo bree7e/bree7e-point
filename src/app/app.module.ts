6dc8f364c090864ccbd128a75cf89ce36cf1810d
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2018-05-14 16:18:22 +0800

    forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +23,7 @@ import { AddEventPageComponent } from './pages/add-event-page/add-event-page.com
         EventComponent,
         AddEventPageComponent
     ],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
--- src/app/pages/add-event-page/add-event-page.component.ts
+++ src/app/pages/add-event-page/add-event-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Output } from '@angular/core';
+
+import { NewsEvent } from 'src/app/shared/timeline-event';
 
 @Component({
   selector: 'app-add-event-page',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event-page.component.scss']
 })
 export class AddEventPageComponent implements OnInit {
+  private newEvent = new NewsEvent('', '', '');
+  
+  @Output() 
+  addTimelineEvent: EventEmitter<NewsEvent> = new EventEmitter();  
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  add() {
+    console.log('addTimelineEvent click')
+  }
+  
 }
